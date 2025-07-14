279cbf7d6841c81e23ef268b0da8fdf0f2a1768f
Leffin <levfag@gmail.com>
2025-07-14 10:29:20 +0700

    init commit

---
+++ Meme/Meme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meme
+//
+//  Created by Effin Leffin on 14.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meme/Meme/MemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeApp.swift
+//  Meme
+//
+//  Created by Effin Leffin on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
